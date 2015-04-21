ad8b26184638eee54639b3ff8e19c39730ee758e
RobertMichalovic <robertmichalovic@seznam.cz>
2015-04-21 11:32:39 +0200

    Uprava č.2

--- src/program/TridaTest.java
+++ src/program/TridaTest.java
@@ -3,6 +3,7 @@ public class TridaTest {
 	public static void main(String[] args) {
 		System.out.println("Start Programu");
 		System.out.println("Úprava è.1");
+		System.out.println("Úprava è.2");
 		
 		System.out.println("Konec Programu");	}
 }
