3b4da403be46f971af4911684d7edeb2d5bb2998
RobertMichalovic <robertmichalovic@seznam.cz>
2015-04-21 11:25:13 +0200

    Uprava č.01

---
+++ src/program/TridaTest.java
@@ -0,0 +1,8 @@
+package program;
+public class TridaTest {
+	public static void main(String[] args) {
+		System.out.println("Start Programu");
+		System.out.println("Úprava è.1");
+		
+		System.out.println("Konec Programu");	}
+}
